aa983b3594f6ba450dd09ab44c898ba304d34a94
Gaobuse <gaobuse.kampfer16@gmail.com>
2023-03-17 12:31:05 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int j;
+
+	for (j = 0 ; j < 10 ; j++)
+		printf("%d", j);
+	printf("\n");
+	return (0);
+}
