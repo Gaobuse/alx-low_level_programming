0ee6d08ea30b79bce15fd354d902e99e73ffcf1f
Gaobuse <gaobuse.kampfer16@gmail.com>
2023-03-22 14:31:55 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result.
+ * @a: first parameter
+ * @b: second parameter
+ * Return: the sum of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
