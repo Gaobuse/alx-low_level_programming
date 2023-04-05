04abce6e2a6f6528ea1fd82fb2db4e3523827512
Gaobuse <gaobuse.kampfer16@gmail.com>
2023-04-05 06:11:12 -0700

    0. memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,19 +4,18 @@
  * _memset - fills memory with a constant byte.
  * @n: bytes of the memory area pointed to by s
  * @s: with the constant byte b
- * @@b: memory pointer
+ * @b: memory pointer
  * Return: a pointer to the memory area s
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
 
-	while (i < n)
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
 	{
 	s[i] = b;
-	i++;
 	}
-
 	return (s);
 }
